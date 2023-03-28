1fd8166491be509d59141ecb0c372e09ff48abe4
Sreedeepta <78677605+sreedeepta@users.noreply.github.com>
2023-03-28 10:38:19 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+class Program8
+{
+    public void calculate(double f)
+    {
+        double c;
+        c=((f-32)/9.0) * 5;
+        System.out.println("Celcius Temperature = " + c);
+    }
+}
+
+
+//Program 10: Finding the area covered by the largest circle inside a square
+class Program10
